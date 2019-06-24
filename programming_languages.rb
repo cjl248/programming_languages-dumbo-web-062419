2dc82abfe71996d666a81cca2f72e0c3fb7786fc
Chris Liendo <cjl248@cornell.edu>
2019-06-24 14:30:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require "pry"
+
+# def reformat_languages(languages)
+#   # your code here
+#   new_hash = {}
+#   languages.each do |k1,v1|
+# 		v1.each do |k2,v2|
+# 		new_hash[v1] = {k2: v1[v2]}
+# 		#binding.pry
+#     end
+#   end
+#   return new_hash
+# end
+
 def reformat_languages(languages)
-  # your code here
+  formatted = {}
+	languages.each do |k, v|
+		v.each do |k2, v2|
+			v2[:style] = [k]
+			formatted[k2] = v2
+		end
+	end
+  return formatted
 end
